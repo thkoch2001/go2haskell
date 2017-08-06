9c10da4af4b12e0c0cf1b6c5d3c8bb2c35af94e9
Thomas Koch <thomas@koch.ro>
2017-08-06 21:38:36 +0200

    .

--- a_tour_of_haskell/methods/errors.hs
+++ a_tour_of_haskell/methods/errors.hs
@@ -1,3 +1,10 @@
+-- Haskell has a couple of mechanism to deal with different categories of errors, even exceptions.
+-- The easiest mechanisms are the Maybe type already used in ... and the Either type used here.
+
+-- I ommited the time in the error type since it would have made the run function impure and thus
+-- more complex. A pure function can not interact with IO and the current time would be an input
+-- from the outside world.
+
 data MyError = MyError String deriving (Show)
 
 run :: String -> Either MyError String
