76dd392c594a241577df395c3c7df59cd280d40b
Thomas Koch <thomas@koch.ro>
2017-08-13 11:17:31 +0200

    .

---
+++ a_tour_of_haskell/concurrency/default-selection.hs
@@ -0,0 +1,38 @@
+-- apt-get install libghc-control-monad-loop-dev
+-- Instead of using two channels and listening on both simultaneously, we use one channel
+-- but exactly define the types of messages we expect to receive over the channel.
+
+import Control.Concurrent (forkIO, threadDelay)
+import Control.Concurrent.STM (atomically)
+import Control.Concurrent.STM.TChan (newTChan, tryReadTChan, writeTChan)
+import Control.Monad (forever)
+import Control.Monad.Trans.Class (lift)
+import Control.Monad.Trans.Loop (exit, repeatLoopT)
+
+
+data Message = Tick | Boom
+
+
+main = do
+  ch <- atomically $ newTChan
+
+  forkIO $ forever $ do
+    threadDelay (10^5)
+    atomically $ writeTChan ch Tick
+
+  forkIO $ do
+    threadDelay (5*10^5)
+    atomically $ writeTChan ch Boom
+
+  repeatLoopT $ do
+    v <- lift $ atomically $ tryReadTChan ch
+    case v of
+      Just Tick -> do
+        lift $ putStrLn "Tick"
+      Just Boom -> do
+        lift $ putStrLn "Boom"
+        exit
+      otherwise -> do
+        lift $ putStrLn "    ."
+        lift $ threadDelay (5*10^4)
+
---
+++ a_tour_of_haskell/concurrency/mutex-counter.hs
@@ -0,0 +1,18 @@
+-- We could use a mutex, but a STM TVar is just so much nicer.
+
+import Control.Concurrent (forkIO, threadDelay)
+import Control.Concurrent.STM (atomically)
+import Control.Concurrent.STM.TVar (newTVar, readTVar, modifyTVar')
+import Control.Monad (forM_)
+import qualified Data.Map.Strict as Map
+
+inc c key = atomically $ modifyTVar' c $ Map.insertWith (+) key 1
+
+main = do
+  c <- atomically $ newTVar (Map.empty::Map.Map String Int)
+  forM_ [1..1000] $ \_ ->
+    forkIO $ inc c "somekey"
+
+  threadDelay (10^6)
+  m <- atomically $ readTVar c
+  print $ Map.findWithDefault 0 "somekey" m
